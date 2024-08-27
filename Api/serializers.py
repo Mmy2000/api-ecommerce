fbfb5e845a11324d1af0ecef7379e91c5a044234
Mmy2000 <yahianada005@gmail.com>
2024-08-28 00:06:37 +0300

    adding endpoints for store app

--- Api/serializers.py
+++ Api/serializers.py
@@ -1,13 +1,16 @@
 from rest_framework import serializers
 from  store.models import Category, Product
 
+class CategorySerializer(serializers.ModelSerializer):
 
+    class Meta:
+        model = Category
+        fields = ["category_id", "title", "slug"]
+
+        
 class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
     class Meta:
         model = Product
         fields = [ "id", "name", "description", "category", "slug", "inventory", "old_price", "price","discount"]
 
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ["category_id", "title", "slug"]
