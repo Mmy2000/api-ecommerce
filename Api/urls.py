7dfaed2f9970221c36a94c82a3a908875a2c997b
Mmy2000 <yahianada005@gmail.com>
2024-08-28 02:08:49 +0300

    adding endpoints using generics for store app

--- Api/urls.py
+++ Api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path("products", views.api_products),
-    path("products/<str:pk>", views.api_product),
-    path("categories", views.api_categories),
-    path("categories/<str:pk>", views.api_category)
+    # path("products", views.api_products),
+    # path("products/<str:pk>", views.api_product),
+    # path("categories", views.api_categories),
+    # path("categories/<str:pk>", views.api_category)
+    path("products" , views.ProductListApi.as_view() , name="products"),
+    path("products/<str:pk>" , views.ProsuctDetailsApi.as_view() , name="product-details"),
 ]
--- Api/views.py
+++ Api/views.py
@@ -6,70 +6,119 @@ from store.models import Category, Product
 from rest_framework.response import Response
 from rest_framework import status
 from Api import serializers
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from Api.filters import ProductFilter
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import generics
+
 
 # Create your views here.
 
-@api_view(['GET', 'POST'])
-def api_products(request):
+# class ProductsViewSet(ModelViewSet):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
     
-    if request.method == 'GET':
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
     
-    if request.method == 'POST':
-        serializer = ProductSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+#     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+#     filterset_class = ProductFilter
+#     search_fields = ['name', 'description']
+#     ordering_fields = ['old_price']
+#     pagination_class = PageNumberPagination
+
+
+# class CategoryViewSet(ModelViewSet):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+
+
+class ProductListApi(generics.ListCreateAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+    # permission_classes = [IsAuthenticated,]
+
+class ProsuctDetailsApi(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ProductSerializer
+    queryset = Product.objects.all()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @api_view(['GET', 'POST'])
+# def api_products(request):
+    
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
+    
+#     if request.method == 'POST':
+#         serializer = ProductSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
 
         
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def api_product(request, pk):
-    product = get_object_or_404(Product, id=pk)
-    if request.method == 'GET':
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def api_product(request, pk):
+#     product = get_object_or_404(Product, id=pk)
+#     if request.method == 'GET':
+#         serializer = ProductSerializer(product)
+#         return Response(serializer.data)
     
-    elif request.method == 'PUT':
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+#     elif request.method == 'PUT':
+#         serializer = ProductSerializer(product, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
     
-    elif request.method == 'DELETE':
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     elif request.method == 'DELETE':
+#         product.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
   
         
 
 
-@api_view(['GET', 'POST'])
-def api_categories(request):
-    if request.method == 'GET':
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
-    if request.method == 'POST':
-        serializer = CategorySerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
+# @api_view(['GET', 'POST'])
+# def api_categories(request):
+#     if request.method == 'GET':
+#         categories = Category.objects.all()
+#         serializer = CategorySerializer(categories, many=True)
+#         return Response(serializer.data)
+#     if request.method == 'POST':
+#         serializer = CategorySerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def api_category(request, pk):
-    category = get_object_or_404(Category, category_id=pk)
-    if request.method == 'GET':
-        serializer = CategorySerializer(category)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = CategorySerializer(category, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    elif request.method == 'DELETE':
-        category.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def api_category(request, pk):
+#     category = get_object_or_404(Category, category_id=pk)
+#     if request.method == 'GET':
+#         serializer = CategorySerializer(category)
+#         return Response(serializer.data)
+#     elif request.method == 'PUT':
+#         serializer = CategorySerializer(category, data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data)
+#     elif request.method == 'DELETE':
+#         category.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
