d2db64129556b7c1aa106f70a48e736e73a56f23
Mmy2000 <yahianada005@gmail.com>
2024-08-27 23:49:27 +0300

    adding store & userProfile models

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'store',
+    'userProfile',
+    'core',
+    'Api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
