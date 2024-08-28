f05287a9cc73ddd908ffff45a74b5988b08a6268
Mmy2000 <yahianada005@gmail.com>
2024-08-28 04:44:47 +0300

    relationship in serializer

--- Api/serializers.py
+++ Api/serializers.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 from  store.models import Category, Product
 
 class CategorySerializer(serializers.ModelSerializer):
-
+    
     class Meta:
         model = Category
         fields = ["category_id", "title", "slug"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # category = CategorySerializer()
+    category = CategorySerializer()
     class Meta:
         model = Product
         fields = [ "id", "name", "description", "category", "slug", "inventory", "old_price", "price","discount","image"]
