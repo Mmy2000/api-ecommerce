3e321c50a39c562517956b88b2d66a2639d3aefa
Mmy2000 <yahianada005@gmail.com>
2024-08-28 04:03:42 +0300

    adding endpoints using viewsets for store app

--- Api/urls.py
+++ Api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path , include
 from . import views
 from rest_framework.routers import DefaultRouter
 
@@ -8,15 +8,16 @@ router.register('categories' , views.CategoryViewSet)
 
 
 
-urlpatterns = router.urls
+# urlpatterns = router.urls
 
-# urlpatterns = [
-#     path("products", views.api_products),
-#     path("products/<str:pk>", views.api_product),
-#     path("categories", views.api_categories),
-#     path("categories/<str:pk>", views.api_category)
-#     path("products" , views.ProductsViewSet.as_view() , name="products"),
-#     path("products/<str:pk>" , views.ProsuctDetailsApi.as_view() , name="product-details"),
-#     path("categories/" , views.CategoriesListApi.as_view() , name="categories"),
-#     path("categories/<str:pk>" , views.CategoriesDetailsApi.as_view() , name="category-details"),
-# ]
+urlpatterns = [
+    path("" , include(router.urls))
+    # path("products", views.api_products),
+    # path("products/<str:pk>", views.api_product),
+    # path("categories", views.api_categories),
+    # path("categories/<str:pk>", views.api_category)
+    # path("products" , views.ProductsViewSet.as_view() , name="products"),
+    # path("products/<str:pk>" , views.ProsuctDetailsApi.as_view() , name="product-details"),
+    # path("categories/" , views.CategoriesListApi.as_view() , name="categories"),
+    # path("categories/<str:pk>" , views.CategoriesDetailsApi.as_view() , name="category-details"),
+]
