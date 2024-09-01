43d283547995ef9bb34ea89129e6337878244db0
Mmy2000 <yahianada005@gmail.com>
2024-09-02 01:52:43 +0300

    get user order items

--- Api/views.py
+++ Api/views.py
@@ -16,7 +16,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination
 from rest_framework import generics
 from rest_framework.parsers import MultiPartParser, FormParser
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 # Create your views here.
 
@@ -71,15 +71,44 @@ class CartItemViewSet(ModelViewSet):
         return {"cart_id": self.kwargs["cart_pk"]}
     
 class OrderViewSet(ModelViewSet):
-    permission_classes = [IsAuthenticated]
-    serializer_class = OrderSerializer
     
+    http_method_names = ["get", "patch", "post", "delete", "options", "head"]
+    
+    def get_permissions(self):
+        if self.request.method in ["PATCH", "DELETE"]:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]
+            
+    
+    
+    def create(self, request, *args, **kwargs):
+        serializer = CreateOrderSerializer(data=request.data, context={"user_id": self.request.user.id})
+        serializer.is_valid(raise_exception=True)
+        order = serializer.save()
+        serializer = OrderSerializer(order)
+        return Response(serializer.data)
+        
+    
+
+    
+    
+    
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return CreateOrderSerializer
+        elif self.request.method == 'PATCH':
+            return UpdateOrderSerializer
+        return OrderSerializer
+       
     
     def get_queryset(self):
         user = self.request.user
         if user.is_staff:
             return Order.objects.all()
         return Order.objects.filter(owner=user)
+    
+    # def get_serializer_context(self):
+    #     return {"user_id": self.request.user.id}
 
 
 class ProfileViewSet(ModelViewSet):
